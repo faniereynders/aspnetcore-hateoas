ba5ee0c0f9ed1cd51aa6031a74b393d84af19419
Fanie Reynders <fanie@reynders.co>
2018-05-29 23:22:40 +0200

    revert changes

--- src/AspNetCore.Hateoas/Infrastructure/MvcBuilderExtensions.cs
+++ src/AspNetCore.Hateoas/Infrastructure/MvcBuilderExtensions.cs
@@ -8,14 +8,14 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class MvcBuilderExtensions
     {
-        public static IMvcBuilder AddHateoas(this IMvcBuilder builder, Action<HateoasOptions> options = null, int order = 0)
+        public static IMvcBuilder AddHateoas(this IMvcBuilder builder, Action<HateoasOptions> options = null)
         {
             if (options != null)
             {
                 builder.Services.Configure(options);
             }
             builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
-            builder.AddMvcOptions(o => o.OutputFormatters.Insert(order ,new JsonHateoasFormatter()));
+            builder.AddMvcOptions(o => o.OutputFormatters.Add(new JsonHateoasFormatter()));
             return builder;
         }
     }
